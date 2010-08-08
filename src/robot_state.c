fb9ba8877855038bb20fdba45261319192d260f1
Trédez Jean-Baptiste <jbtredez@gmail.com>
2010-08-08 23:10:06 +0200

    robot_state : critical section
    	modified:   src/robot_state.c

--- src/robot_state.c
+++ src/robot_state.c
@@ -10,8 +10,6 @@
 
 static uint16_t encoders_right;
 static uint16_t encoders_left;
-static uint16_t encoders_right_old;
-static uint16_t encoders_left_old;
 
 static float v_distance;   //!< en "m / unité de temps"
 static float v_rotate;     //!< en "rd / unité de temps"
@@ -22,8 +20,6 @@ static int robot_state_module_init()
 {
 	encoders_right = encoders_get_right();
 	encoders_left = encoders_get_left();
-	encoders_right_old = encoders_right;
-	encoders_left_old = encoders_left;
 
 	pos.x = 0;
 	pos.y = 0;
@@ -36,36 +32,45 @@ module_init(robot_state_module_init, INIT_ROBOT_STATE);
 
 void robot_state_update_odometry()
 {
-	//! @todo lock
-	encoders_right = encoders_get_right();
-	encoders_left = encoders_get_left();
+	portENTER_CRITICAL();
+	uint16_t enc_right_old = encoders_right;
+	uint16_t enc_left_old  = encoders_left;
+	portEXIT_CRITICAL();
 
-	float delta_right = (float) ((int16_t) (encoders_right - encoders_right_old) );
-	float delta_left  = (float) ((int16_t) (encoders_left  - encoders_left_old ) );
+	uint16_t enc_right = encoders_get_right();
+	uint16_t enc_left = encoders_get_left();
+
+	float delta_right = (float) ((int16_t) (enc_right - enc_right_old) );
+	float delta_left  = (float) ((int16_t) (enc_left  - enc_left_old ) );
 
 	delta_right *= PARAM_RIGHT_ODO_WHEEL_RADIUS * PARAM_RIGHT_ODO_WHEEL_WAY;
 	delta_left  *= PARAM_LEFT_ODO_WHEEL_RADIUS  * PARAM_LEFT_ODO_WHEEL_WAY;
 
-	v_distance = PARAM_DIST_ODO_GAIN * (delta_right + delta_left);
-	v_rotate   = PARAM_ROT_ODO_GAIN  * (delta_right - delta_left);
+	float v_d = PARAM_DIST_ODO_GAIN * (delta_right + delta_left);
+	float v_r = PARAM_ROT_ODO_GAIN  * (delta_right - delta_left);
 
-	//! @todo voir / unités : en m/s, v_distance = v_distance / te
-	//! @todo voir / unités : en m/s, v_rotate   = v_rotate / te
+	float ca = cos(pos.alpha);
+	float dx = v_d * ca;
+	float dy = v_d * ca;
+	float da = v_r;
 
-	encoders_right_old = encoders_right;
-	encoders_left_old  = encoders_left;
-
-	pos.x += v_distance * cos(pos.alpha);
-	pos.y += v_distance * cos(pos.alpha);
-	pos.alpha += v_rotate;
-	//! @todo unlock
+	// update
+	portENTER_CRITICAL();
+	encoders_right = enc_right;
+	encoders_left  = enc_left;
+	v_distance = v_d;
+	v_rotate   = v_r;
+	pos.x += dx;
+	pos.y += dy;
+	pos.alpha += da;
+	portEXIT_CRITICAL();
 }
 
 struct vect_pos robot_state_get_position()
 {
 	struct vect_pos p;
-	//! @todo lock
+	portENTER_CRITICAL();
 	p = pos;
-	//! @todo unlock
+	portEXIT_CRITICAL();
 	return p;
 }
