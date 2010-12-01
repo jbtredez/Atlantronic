81f331b0166accb0a4278be7a3a3f2d1d3431a9e
Trédez Jean-Baptiste <jbtredez@gmail.com>
2010-12-01 22:41:04 +0100

    demenagement simu
    	renamed:    ../src/tools/simu/3ds.cxx -> 3ds.cxx
    	renamed:    ../src/tools/simu/3ds.h -> 3ds.h
    	new file:   Makefile
    	renamed:    ../src/tools/simu/MatriceHomogene.cxx -> MatriceHomogene.cxx
    	renamed:    ../src/tools/simu/MatriceHomogene.h -> MatriceHomogene.h
    	renamed:    ../src/tools/simu/log.h -> log.h
    	renamed:    ../src/tools/simu/modele3D.cxx -> modele3D.cxx
    	renamed:    ../src/tools/simu/modele3D.h -> modele3D.h
    	renamed:    ../src/tools/simu/simu.cxx -> simu.cxx
    	new file:   stm32f10x.h
    	renamed:    ../src/tools/simu/table.3ds -> table.3ds
    	renamed:    ../src/tools/simu/tomate.3ds -> tomate.3ds
    	renamed:    ../src/tools/simu/vecteur.h -> vecteur.h
    	deleted:    ../src/tools/simu/Makefile

--- simu/simu.cxx
+++ simu/simu.cxx
@@ -49,14 +49,76 @@ static void infoCG(GtkWidget* widget, gpointer data); //!< affiche et s'occupe d
 static int fd_to_qemu;
 static int fd_to_simu;
 
+// tests
+#if 0
+#include <pthread.h>
+static pthread_t id;
+
+#define ACK               1
+#define WRITE_MEMORY      2
+#define READ_MEMORY       3
+
+struct atlantronic_memory_io
+{
+	uint8_t cmd;
+	uint64_t offset;
+	uint32_t val;
+};
+
+
+void* lecture(void* arg)
+{
+	struct atlantronic_memory_io io;
+	int n;
+	uint8_t ack = ACK;
+
+	while(1)
+	{
+		n = read(fd_to_simu, &io, sizeof(io) );
+		if( n == sizeof(io))
+		{
+			if(io.cmd == WRITE_MEMORY)
+			{
+				write(fd_to_qemu, &ack, sizeof(ack));
+				printf("commande : %i, offset : %#.4x val : %#.2x\n", io.cmd, io.offset, io.val);
+			}
+			else if(io.cmd == READ_MEMORY)
+			{
+				io.val = 10;
+				write(fd_to_qemu, &io, sizeof(io));
+			}
+			else
+			{
+				printf("erreur protocole\n");
+				return NULL;
+			}
+		}
+		else if(n < 0)
+		{
+			perror("read");
+			return NULL;
+		}
+		else
+		{
+			printf("erreur protocole\n");
+			return NULL;
+		}
+	}
+
+	return NULL;
+}
+#endif
 int main(int argc, char** argv)
 {
-/*
+#if 0
 	mkfifo("/tmp/to_qemu", 0666);
 	mkfifo("/tmp/to_simu", 0666);
-	fd_to_qemu = open("/tmp/to_qemu", O_WRONLY | O_SYNC);
-	fd_to_simu = open("/tmp/to_simu", O_RDONLY | O_SYNC);
-*/
+
+	fd_to_qemu = open("/tmp/to_qemu", O_WRONLY);
+	fd_to_simu = open("/tmp/to_simu", O_RDONLY);
+
+	pthread_create(&id, NULL, lecture, NULL);
+#endif
 	if( ! g_thread_supported() )
 	{
 		g_thread_init(NULL);
