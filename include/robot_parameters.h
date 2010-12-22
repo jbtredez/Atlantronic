0d68e9343c19ca745c2115c673668944f7c861a5
Trédez Jean-Baptiste <jbtredez@gmail.com>
2010-12-22 22:55:10 +0100

    bug / nan sur l'odometry (arm)
    	modified:   include/robot_parameters.h

--- include/robot_parameters.h
+++ include/robot_parameters.h
@@ -13,7 +13,7 @@
 #define PARAM_LEFT_MOT_WHEEL_RADIUS           50.0f
 
 #define PARAM_RIGHT_ODO_WHEEL_WAY                 1
-#define PARAM_LEFT_ODO_WHEEL_WAY                 -1
+#define PARAM_LEFT_ODO_WHEEL_WAY                  1
 #define PARAM_RIGHT_MOT_WHEEL_WAY                 1
 #define PARAM_LEFT_MOT_WHEEL_WAY                  1
 
@@ -25,10 +25,10 @@
 #define PARAM_MOT_RED                            21
 #define PARAM_ODO_RED                             1
 
-#define PARAM_DIST_ODO_GAIN                (M_PI / (PARAM_ODO_RED * PARAM_ENCODERS_RES) )
-#define PARAM_ROT_ODO_GAIN                 (2.0f * M_PI / (PARAM_ENCODERS_RES * PARAM_VOIE_ODO * PARAM_ODO_RED))
-#define PARAM_DIST_MOD_GAIN                (1.0f / (2.0f * PARAM_MOT_RED))
-#define PARAM_ROT_MOD_GAIN                 (1.0f / (PARAM_VOIE_MOT * PARAM_MOT_RED))
+#define PARAM_DIST_ODO_GAIN                (float) (M_PI / (PARAM_ODO_RED * PARAM_ENCODERS_RES) )
+#define PARAM_ROT_ODO_GAIN                 (float) (2.0f * M_PI / (PARAM_ENCODERS_RES * PARAM_VOIE_ODO * PARAM_ODO_RED))
+#define PARAM_DIST_MOD_GAIN                (float) (1.0f / (2.0f * PARAM_MOT_RED))
+#define PARAM_ROT_MOD_GAIN                 (float) (1.0f / (PARAM_VOIE_MOT * PARAM_MOT_RED))
 
 
 #endif
