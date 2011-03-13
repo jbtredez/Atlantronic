82968e22553753f3b7319a8c21206ca93580f8f5
Trédez Jean-Baptiste <jbtredez@gmail.com>
2011-03-13 11:46:06 +0100

    codeurs : mise au propre
    	modified:   src/io/arm_cm3/encoders.c

--- src/io/arm_cm3/encoders.c
+++ src/io/arm_cm3/encoders.c
@@ -16,8 +16,8 @@ static int encoders_module_init()
 	RCC->APB2ENR |=  RCC_APB2ENR_IOPAEN;
 	GPIOA->CRL   &= ~(GPIO_CRL_MODE0 | GPIO_CRL_CNF0 | GPIO_CRL_MODE1 | GPIO_CRL_CNF1);     // on efface la conf de PA0 et PA1
 
+	// activation de la clock sur le timer 2
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
-	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
 
 	TIM2->CR1 = 0x00;
 	TIM2->CR2 = 0x00;
@@ -34,22 +34,27 @@ static int encoders_module_init()
 
 	// pas d'inversions de polarité
 	TIM2->CCER = 0x00;
-	TIM2->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E;
 
-	TIM2->CCR1 = 0;
+	// mise a zero
+	TIM2->CNT = 0x00;
 
 	// on active le tout
 	TIM2->CR1 |= TIM_CR1_CEN;
 
+	// activation de la clock sur AFIO
+	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+
 	// Timer 4 : remap : PD12, PD13, PD14, PD15
 	AFIO->MAPR |= AFIO_MAPR_TIM4_REMAP;
-	TIM4->CCR1 = 0;
 
 	// GPIOD utilisee, configuration de PD12 et PD13
 	RCC->APB2ENR |=  RCC_APB2ENR_IOPDEN;
 	GPIOA->CRH   &= ~(GPIO_CRH_MODE12 | GPIO_CRH_CNF12 | GPIO_CRH_MODE13 | GPIO_CRH_CNF13);     // on efface la conf de PD12 et PD13
 	GPIOA->CRH   |=  GPIO_CRH_CNF12_1;               // PD12 : input pull-up / pull-down
 	GPIOA->CRH   |=  GPIO_CRH_CNF13_1;               // PD13 : input pull-up / pull-down
+
+	// activation de la clock sur le timer 4
+	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
 
 	TIM4->CR1 = 0x00;
 	TIM4->CR2 = 0x00;
@@ -66,9 +71,9 @@ static int encoders_module_init()
 
 	// pas d'inversions de polarité
 	TIM4->CCER = 0x00;
-	TIM4->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E;
 
-	TIM4->CCR1 = 0;
+	// mise a zero
+	TIM4->CNT = 0x00;
 
 	// on active le tout
 	TIM4->CR1 |= TIM_CR1_CEN;
