0e0ba3c06d1bb9914b9137a7efcfa3035bd99c62
Trédez Jean-Baptiste <jbtredez@gmail.com>
2011-03-15 19:52:04 +0100

    codes d'erreur avec la led warning
    	modified:   include/error.h

--- include/error.h
+++ include/error.h
@@ -6,21 +6,22 @@
 
 // code 0x00 réservé : fin du match
 
-#define ERR_NMI                 0x01
-#define ERR_HARD_FAULT          0x02
-#define ERR_MPU_FAULT           0x03
-#define ERR_BUS_FAULT           0x04
-#define ERR_USAGE_FAULT         0x05
-#define ERR_UNEXPECTED_ISR      0x06
-#define ERR_DEBUG_MONITOR       0x07
+#define ERR_NMI                 0x200
+#define ERR_ARU                 0x201      //!< 2 led rouges
+#define ERR_HARD_FAULT          0x202
+#define ERR_MPU_FAULT           0x203
+#define ERR_BUS_FAULT           0x204
+#define ERR_USAGE_FAULT         0x205
+#define ERR_UNEXPECTED_ISR      0x206
+#define ERR_DEBUG_MONITOR       0x207
 
-#define ERR_SYSTICK             0x08
-#define ERR_INIT_LOG            0x09
-#define ERR_INIT_AX12           0x0a
-#define ERR_INIT_CONTROL        0x0b
-#define ERR_INIT_TEST           0x0c
-#define ERR_INIT_END            0x0d
+#define ERR_SYSTICK             0x208
+#define ERR_INIT_LOG            0x209
+#define ERR_INIT_AX12           0x20a
+#define ERR_INIT_CONTROL        0x20b
+#define ERR_INIT_TEST           0x20c
+#define ERR_INIT_END            0x20d
 
-#define ERR_ARU                0x201      //!< 2 led rouges
+
 
 #endif
