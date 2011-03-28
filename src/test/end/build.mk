8399153a164a908c82b38cc61d78aef4803bb559
Trédez Jean-Baptiste <jbtredez@gmail.com>
2011-03-28 19:18:24 +0200

    rangement (suite)
    	renamed:    src/arch/isr.c -> src/asm/isr.c
    	renamed:    src/arch/adc.c -> src/io/adc.c
    	renamed:    src/arch/gpio.c -> src/io/gpio.c
    	modified:   src/test/build.mk
    	modified:   src/test/end/build.mk
    	modified:   src/test/led/build.mk
    	modified:   src/test/task_1/build.mk

--- src/test/end/build.mk
+++ src/test/end/build.mk
@@ -3,14 +3,14 @@ obj-test_end += end.o
 obj-test_end += syscalls.o
 obj-test_end += test/end/task1.o
 obj-test_end += io/rcc.o
-obj-test_end += arch/gpio.o
-obj-test_end += arch/adc.o
+obj-test_end += io/gpio.o
+obj-test_end += io/adc.o
 obj-test_end += io/systick.o
 obj-test_end += rtos/list.o
 obj-test_end += rtos/tasks.o
 obj-test_end += rtos/portable/MemMang/heap_2.o
 obj-test_end += rtos/portable/GCC/ARM_CM3/port.o
-obj-test_end += arch/isr.o
+obj-test_end += asm/isr.o
 lib-led += -lm
 BIN-arm_cm3 += test_end
 
--- src/test/task_1/build.mk
+++ src/test/task_1/build.mk
@@ -3,13 +3,13 @@ obj-test_task_1 += syscalls.o
 obj-test_task_1 += test/task_1/task1.o
 obj-test_task_1 += test/task_1/task2.o
 obj-test_task_1 += io/rcc.o
-obj-test_task_1 += arch/gpio.o
+obj-test_task_1 += io/gpio.o
 obj-test_task_1 += io/systick.o
 obj-test_task_1 += rtos/list.o
 obj-test_task_1 += rtos/tasks.o
 obj-test_task_1 += rtos/portable/MemMang/heap_2.o
 obj-test_task_1 += rtos/portable/GCC/ARM_CM3/port.o
-obj-test_task_1 += arch/isr.o
+obj-test_task_1 += asm/isr.o
 
 lib-test_task_1 += -lm
 BIN-arm_cm3 += test_task_1
