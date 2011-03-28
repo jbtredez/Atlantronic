a3e7a637cca6931fee47fb8b52f414ef613ff5a1
Trédez Jean-Baptiste <jbtredez@gmail.com>
2011-03-29 00:05:12 +0200

    config CAN à 500 kb/s, sp à 72,22% SJW=4, TBS1=12TQ, TBS2=5TQ
    	modified:   src/io/can.c

--- src/io/can.c
+++ src/io/can.c
@@ -1,6 +1,6 @@
 #include "io/can.h"
 #include "module.h"
-
+#include "io/rcc.h"
 
 static int can_module_init(void)
 {
@@ -24,8 +24,33 @@ static int can_module_init(void)
 	// activation clock sur le can 1
 	RCC->APB1ENR |= RCC_APB1ENR_CAN1EN;
 
-	// TODO : activer it
-	// TODO : CAN->MCR et CAN->BTR
+	// init mode
+	CAN1->MCR = CAN_MCR_INRQ;
+
+	#if( RCC_PCLK1 != 36000000)
+	#error "remettre RCC_PCLK1 à 36Mhz, sinon c'est le bordel pour recalculer BTR"
+	#endif
+
+	// TBS1 = 12 TQ   
+	// TBS2 =  5 TQ
+	// SJW  =  4 TQ
+	// total bit can (1 + TBS1 + TBS2) = 18 TQ
+	// SP = (1 + TBS1)/total = 72,22 %
+	// vitesse : 500kb => 500000*18*TQ = PCLK = 36Mhz
+	// => TQ = PCLK / (18 * 500000) = 4
+	CAN1->BTR &= ~ (              CAN_BTR_SJW  |                  CAN_BTR_TS2  |                   CAN_BTR_TS1  |   CAN_BTR_BRP    );
+	CAN1->BTR |= (((4-1) << 24) & CAN_BTR_SJW) | (((5-1) << 20) & CAN_BTR_TS2) | (((12-1) << 16) & CAN_BTR_TS1) | ((4-1) & CAN_BTR_BRP);
+
+	// TODO à voir / it... + handler
+	//CAN1->IER
+	//NVIC_EnableIRQ(CAN1_TX_IRQn);
+	//NVIC_EnableIRQ(CAN1_RX0_IRQn);
+
+	// lancement du CAN
+	CAN1->MCR &= ~CAN_MCR_INRQ;
+	//while (CAN->MSR & CAN_MCR_INRQ) ;
+
+// TODO filtres / messages
 
 	return 0;
 }
