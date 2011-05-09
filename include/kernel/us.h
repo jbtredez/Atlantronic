0816bca7df375fc3619d352d78ce9fcc36e247af
Trédez Jean-Baptiste <jbtredez@gmail.com>
2011-05-10 00:05:07 +0200

    can - message us
    	modified:   include/kernel/us.h
    	modified:   src/kernel/can_us.c

--- include/kernel/us.h
+++ include/kernel/us.h
@@ -5,5 +5,19 @@
 //! @brief CAN - US
 //! @author Atlantronic
 
+#include <stdint.h>
+
+//! TODO : trouver un nom selon la position
+enum us_id
+{
+	US_1,
+	US_2,
+	US_3,
+	US_4,
+	US_5,
+	US_MAX
+};
+
+uint32_t us_get_state(enum us_id id);
 
 #endif
--- src/kernel/can_us.c
+++ src/kernel/can_us.c
@@ -8,6 +8,10 @@
 #include "kernel/driver/can.h"
 #include "kernel/module.h"
 #include "kernel/can_id.h"
+#include "kernel/portmacro.h"
+#include <string.h>
+
+uint32_t can_us_state[US_MAX];
 
 void can_us_callback(struct can_msg *msg);
 
@@ -22,5 +26,29 @@ module_init(can_us_module_init, INIT_CAN_US);
 
 void can_us_callback(struct can_msg *msg)
 {
+	if(msg->data[0] < US_MAX && msg->size == 5)
+	{
+		portENTER_CRITICAL();
+		memcpy(&can_us_state, msg->data + 1, 4);
+		portEXIT_CRITICAL();
+	}
+	else
+	{
+		// erreur TODO log
+		// on fait rien, attente du prochain message CAN
+	}
+}
 
+uint32_t us_get_state(enum us_id id)
+{
+	uint32_t res = 0;
+
+	if(id < US_MAX)
+	{
+		portENTER_CRITICAL();
+		res = can_us_state[id];
+		portEXIT_CRITICAL();
+	}
+
+	return res;
 }
