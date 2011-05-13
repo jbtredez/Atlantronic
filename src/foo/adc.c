6628127fe077e47159f36c754eb4104c7af64ed5
Trédez Jean-Baptiste <jbtredez@gmail.com>
2011-05-13 19:02:21 +0200

    ajout des AN manquantes
    	modified:   src/foo/adc.c

--- src/foo/adc.c
+++ src/foo/adc.c
@@ -9,24 +9,36 @@ static void adc_task(void* arg);
 struct
 {
 	volatile uint16_t potard1;
-	volatile uint16_t aru;
 	volatile uint16_t potard2;
+	volatile uint16_t i1;
+	volatile uint16_t i2;
+	volatile uint16_t i3;
+	volatile uint16_t i4;
+	volatile uint16_t vBat1;
+	volatile uint16_t vBatAru;
 } adc_dma ;
 
 int adc_module_init()
 {
 	// activation de GPIOA
 	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-	// PA4 entrée AN4
-	GPIOA->CRL = (GPIOA->CRL & ~GPIO_CRL_MODE4 & ~GPIO_CRL_CNF4);
 
-	// PC0 (AN10) et PC4 (AN14)
+	GPIOA->CRL = GPIOA->CRL & ~ (
+	                  GPIO_CRL_MODE4 | GPIO_CRL_CNF4 |  // PA4 entrée AN4
+	                  GPIO_CRL_MODE5 | GPIO_CRL_CNF5    // PA5 entrée AN5
+	             );
+
 	// activation de GPIOC
 	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	// PC0 entrée AN10
-	GPIOC->CRL = (GPIOC->CRL & ~GPIO_CRL_MODE0 & ~GPIO_CRL_CNF0);
-	// PC4 entrée AN14
-	GPIOC->CRL = (GPIOC->CRL & ~GPIO_CRL_MODE4 & ~GPIO_CRL_CNF4);
+
+	GPIOC->CRL = GPIOC->CRL & ~(
+	                  GPIO_CRL_MODE0 | GPIO_CRL_CNF0 |  // PC0 entrée AN10
+	                  GPIO_CRL_MODE1 | GPIO_CRL_CNF1 |  // PC1 entrée AN11
+	                  GPIO_CRL_MODE2 | GPIO_CRL_CNF2 |  // PC2 entrée AN12
+	                  GPIO_CRL_MODE3 | GPIO_CRL_CNF3 |  // PC3 entrée AN13
+	                  GPIO_CRL_MODE4 | GPIO_CRL_CNF4 |  // PC4 entrée AN14
+	                  GPIO_CRL_MODE5 | GPIO_CRL_CNF5    // PC5 entrée AN15
+                 );
 
 	// activation clock adc 1 et adc 2
 	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
@@ -44,15 +56,26 @@ int adc_module_init()
 
 	ADC1->SQR1 = 0x00;
 	ADC1->SQR2 = 0x00;
-	ADC1->SQR3 =                  ADC_SQR3_SQ1_2                                   | // 0100 : AN4
-	 			 ADC_SQR3_SQ2_3 |                  ADC_SQR3_SQ2_1                  | // 1010 : AN10
-	 			 ADC_SQR3_SQ3_3 | ADC_SQR3_SQ3_2 | ADC_SQR3_SQ3_1                 ;  // 1110 : AN14
+	ADC1->SQR3 =                  ADC_SQR3_SQ1_2                                   | // 0100 : AN4  (potentiomètre 1)
+	 			 ADC_SQR3_SQ2_3 | ADC_SQR3_SQ2_2 | ADC_SQR3_SQ2_1                  | // 1110 : AN14 (potentiomètre 2)
+ 	 			 ADC_SQR3_SQ3_3 | ADC_SQR3_SQ3_2 | ADC_SQR3_SQ3_1 | ADC_SQR3_SQ3_0 | // 1111 : AN15 (intensité moteur 1)
+ 	 			                  ADC_SQR3_SQ4_2 |                  ADC_SQR3_SQ4_0 | // 0101 : AN5  (intensité moteur 2)
+ 	 			 ADC_SQR3_SQ5_3 | ADC_SQR3_SQ5_2 |                  ADC_SQR3_SQ5_0 | // 1101 : AN13 (intensité moteur 3)
+ 	 			 ADC_SQR3_SQ6_3 |                  ADC_SQR3_SQ6_1 | ADC_SQR3_SQ6_0 ; // 1011 : AN11 (intensité moteur 4)
 
-	ADC1->SMPR2 = (ADC1->SMPR2 & ~ADC_SMPR2_SMP4) | ADC_SMPR2_SMP4; // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN5
-	ADC1->SMPR1 = (ADC1->SMPR1 & ~ADC_SMPR1_SMP10) | ADC_SMPR1_SMP10; // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN11
-	ADC1->SMPR1 = (ADC1->SMPR1 & ~ADC_SMPR1_SMP14) | ADC_SMPR1_SMP14; // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN13
+	ADC1->SQR2 = ADC_SQR2_SQ7_3 | ADC_SQR2_SQ7_2                                   | // 1100 : AN12 (vBat1)
+	             ADC_SQR2_SQ8_3 |                  ADC_SQR2_SQ8_1                  ; // 1010 : AN10 (vBatAru)
 
-	ADC1->SQR1 = (ADC1->SQR1 & ~ADC_SQR1_L) | ADC_SQR1_L_1; // 3 conversions - 1
+	ADC1->SMPR2 = ADC_SMPR2_SMP4  | // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN4 (potentiomètre 1)
+	              ADC_SMPR2_SMP5  ; // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN5 (intensité moteur 2)
+	ADC1->SMPR1 = ADC_SMPR1_SMP10 | // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN10 (vBatAru)
+	              ADC_SMPR1_SMP11 | // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN11 (intensité moteur 4)
+	              ADC_SMPR1_SMP12 | // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN12 (vBat1)
+	              ADC_SMPR1_SMP13 | // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN13 (intensité moteur 3)
+	              ADC_SMPR1_SMP14 | // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN14 (potentiomètre 2)
+	              ADC_SMPR1_SMP15 ; // 1.5 cycle (à 12Mhz) pour l'échantillonage de AN15 (intensité moteur 1)
+
+	ADC1->SQR1 = (ADC1->SQR1 & ~ADC_SQR1_L) | ADC_SQR1_L_2 | ADC_SQR1_L_1 | ADC_SQR1_L_0; // 8 conversions - 1
 
 	// dma
 	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
@@ -64,7 +87,7 @@ int adc_module_init()
 	// mode circulaire
 	// transfert : mem périph => mem
 	DMA1_Channel1->CCR = DMA_CCR1_MSIZE_0 | DMA_CCR1_PSIZE_0 | DMA_CCR1_MINC | DMA_CCR1_CIRC | DMA_CCR1_TCIE;
-	DMA1_Channel1->CNDTR = (0x03) & DMA_CNDTR1_NDT;
+	DMA1_Channel1->CNDTR = (0x08) & DMA_CNDTR1_NDT;
 	DMA1_Channel1->CPAR = (uint32_t) &ADC1->DR;
 	DMA1_Channel1->CMAR = (uint32_t) & adc_dma;
 
@@ -101,7 +124,7 @@ void isr_dma1_channel1(void)
 	if( DMA1->ISR | DMA_ISR_TCIF1)
 	{
 		DMA1->IFCR |= DMA_IFCR_CTCIF1;
-		if(adc_dma.aru == 0)// TODO val + bateries
+		if(adc_dma.vBatAru == 0)// TODO val + bateries
 		{
 			setLed(ERR_ARU);
 		}
