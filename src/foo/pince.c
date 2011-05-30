5942a2b73975494435960425e28670ed84066a0b
Trédez Jean-Baptiste <jbtredez@gmail.com>
2011-05-30 22:07:17 +0200

    couple des ax12

--- src/foo/pince.c
+++ src/foo/pince.c
@@ -10,8 +10,8 @@ void pince_configure()
 	ax12_set_moving_speed(PINCE_AX12_RIGHT, 0x3ff);
 	ax12_set_moving_speed(PINCE_AX12_LEFT, 0x3ff);
 
-	ax12_set_torque_limit(PINCE_AX12_RIGHT, 0x2ff);
-	ax12_set_torque_limit(PINCE_AX12_LEFT, 0x2ff);
+	ax12_set_torque_limit(PINCE_AX12_RIGHT, 0x3ff);
+	ax12_set_torque_limit(PINCE_AX12_LEFT, 0x3ff);
 
 	ax12_set_torque_enable(PINCE_AX12_RIGHT, 1);
 	ax12_set_torque_enable(PINCE_AX12_LEFT, 1);
