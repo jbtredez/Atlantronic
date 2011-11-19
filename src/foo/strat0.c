e857e3b316de535ea3881b77511aabd87cf53abb
Trédez Jean-Baptiste <jbtredez@gmail.com>
2011-11-19 19:53:10 +0100

    suppression des anciennes strategies - debut strat0

---
+++ src/foo/strat0.c
@@ -0,0 +1,63 @@
+#include "kernel/FreeRTOS.h"
+#include "kernel/task.h"
+#include "kernel/module.h"
+#include "control/control.h"
+#include "kernel/systick.h"
+#include "kernel/event.h"
+#include "gpio.h"
+#include "location/location.h"
+#include "kernel/rcc.h"
+#include <math.h>
+#include "kernel/robot_parameters.h"
+#include "pince.h"
+#include "control/control_pince.h"
+#include "recalage.h"
+#include "kernel/hokuyo_tools.h"
+#include "us.h"
+
+#define STRAT_STACK_SIZE       300
+
+static void strat_task();
+int strat_module_init();
+int pos_tour;
+
+int strat_module_init()
+{
+	xTaskHandle xHandle;
+	portBASE_TYPE err = xTaskCreate(strat_task, "strat0", STRAT_STACK_SIZE, NULL, PRIORITY_TASK_STRATEGY, &xHandle);
+
+	if(err != pdPASS)
+	{
+		return ERR_INIT_TEST;
+	}
+
+	return 0;
+}
+
+module_init(strat_module_init, INIT_STRATEGY);
+
+static void strat_task()
+{
+	int sens = 1;
+
+	while(getGo() == 0)
+	{
+		if( getRecalage() )
+		{
+			recalage();
+			resetRecalage();
+		}
+		vTaskDelay(ms_to_tick(50));
+	}
+
+	vTaskWaitEvent(EVENT_GO, portMAX_DELAY);
+
+	if(getcolor() != COLOR_BLUE)
+	{
+		sens = -1;
+	}
+
+	pince_open();
+
+	vTaskDelete(NULL);
+}
