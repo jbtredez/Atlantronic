97f0c6415072c8665588a73d66d10980c991a14f
Trédez Jean-Baptiste <jbtredez@gmail.com>
2012-01-30 20:46:09 +0100

    menage

--- include/kernel/hokuyo_tools.h
+++ include/kernel/hokuyo_tools.h
@@ -9,14 +9,6 @@
 #include "kernel/driver/hokuyo.h"
 #include "kernel/vect_pos.h"
 
-#define NB_PION 19*2
-
-#define VIDE 'V'
-#define PION 'P'
-#define FIGURE 'F'
-#define TOUR 'T'
-#define AUTRE 'A'
-
 struct hokuyo_object
 {
 	uint16_t start;
@@ -30,20 +22,5 @@ void hokuyo_compute_xy(struct hokuyo_scan* scan, struct fx_vect2 *pos, struct fx
 int hokuyo_find_objects(uint16_t* distance, unsigned int size, struct hokuyo_object* obj, unsigned int obj_size);
 
 int hokuyo_object_is_pawn(uint16_t* distance, struct hokuyo_object* obj, struct vect_pos *pawn_pos);
-
-void hoku_init_pion(void);
-void hoku_get_pion(uint16_t index, unsigned char *objet, float* x, float* y, int64_t *timestamp);
-
-void parse_before_match_tab();
-
-uint8_t hoku_check_path();
-
-uint8_t check_shape(uint16_t* distance, unsigned int start, unsigned int end);
-
-void hoku_pion_table_verify_pawn(struct vect_pos *pPosRobot);
-
-uint16_t distance_forward_shape(uint16_t* distance, unsigned int size);
-
-int is_pawn_front_start();
 
 #endif
