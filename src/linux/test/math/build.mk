82133b699a859d4e9f2a845c7d84153778f55d58
Trédez Jean-Baptiste <jbtredez@gmail.com>
2012-02-03 07:56:59 +0100

    demenagement du programme de test segment_intersection dans math

--- src/linux/test/math/build.mk
+++ src/linux/test/math/build.mk
@@ -1,3 +1,8 @@
+obj-linux-test_segment_intersection += kernel/math/segment_intersection.o
+obj-linux-test_segment_intersection += linux/test/math/test_segment_intersection.o
+lib-linux-test_segment_intersection += -lm
+bin-linux += test_segment_intersection
+
 obj-linux-test_trigo += kernel/math/trigo.o
 obj-linux-test_trigo += linux/test/math/test_trigo.o
 lib-linux-test_trigo += -lm
--- src/linux/test/segment_intersection/build.mk
+++
@@ -1,6 +0,0 @@
-obj-linux-test_segment_intersection += kernel/math/segment_intersection.o
-obj-linux-test_segment_intersection += linux/test/segment_intersection/test_segment_intersection.o
-
-lib-linux-test_segment_intersection += -lm
-
-bin-linux += test_segment_intersection
