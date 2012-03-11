644557641063fff95f02d27003f1d1c0997d4fda
Trédez Jean-Baptiste <jbtredez@gmail.com>
2012-03-11 18:26:39 +0100

    doc (pb / unités)

--- include/kernel/math/kinematics.h
+++ include/kernel/math/kinematics.h
@@ -11,11 +11,11 @@ struct kinematics
 {
 	int32_t x;          //!< coordonnée selon l'axe x en 2^-16 mm
 	int32_t y;          //!< coordonnée selon l'axe y en 2^-16 mm
-	int32_t alpha;      //!< orientation en 2^-24 tours
+	int32_t alpha;      //!< orientation en 2^-26 tours
 	int32_t ca;         //!< fx_cos(alpha)
 	int32_t sa;         //!< fx_sin(alpha)
-	int32_t v;          //!< vitesse d'avance en 2^-16 mm/s
-	int32_t w;          //!< vitesse de rotation en 2^-24 tours / s
+	int32_t v;          //!< vitesse d'avance en 2^-16 mm / unite
+	int32_t w;          //!< vitesse de rotation en 2^-26 tours / unite
 };
 
 #endif
