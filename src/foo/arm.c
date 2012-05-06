86c5d5bd25529e4ecad5ec456575f2a0931d8d63
Trédez Jean-Baptiste <jbtredez@gmail.com>
2012-05-06 16:46:32 +0200

    arm - correction calculs + mise en place des limitations angulaires

--- src/foo/arm.c
+++ src/foo/arm.c
@@ -18,6 +18,7 @@
 #include "foo/pwm.h"
 #include "ax12.h"
 #include <math.h>
+#include <stdlib.h>
 
 #define ARM_STACK_SIZE       300
 #define ARM_STEP_BY_MM         5
@@ -40,14 +41,14 @@ const int32_t ARM_DMAX = (400 << 16) / (ARM_HZ * ARM_HZ);
 
 //!< position de la base du bras dans le repère robot
 const struct fx_vect_pos ARM_BASE = {
-	.x = 100,
+	.x = (110 << 16),
 	.y = 0,
 	.alpha = 0,
-	.ca = 1,
+	.ca = 1 << 30,
 	.sa = 0
 };
 
-static const int32_t ARM_L1 = 110 << 16;
+static const int32_t ARM_L1 = 118 << 16;
 static const int32_t ARM_L2 = 120 << 16;
 
 //!< modèle géométrique inverse du bras
@@ -74,9 +75,8 @@ static int arm_module_init()
 		return ERR_INIT_ARM;
 	}
 
-	// TODO : mettre les limites
-	ax12_set_goal_limit(AX12_ARM_1, 0, 0x3ff);
-	ax12_set_goal_limit(AX12_ARM_2, 0, 0x3ff);
+	ax12_set_goal_limit(AX12_ARM_1, 0xcc, 0x332);
+	ax12_set_goal_limit(AX12_ARM_2, 0x00, 0x3ff);
 
 	usb_add_cmd(USB_CMD_ARM_XYZ, &arm_cmd_xyz);
 	usb_add_cmd(USB_CMD_ARM_ZAB, &arm_cmd_zab);
@@ -89,6 +89,16 @@ module_init(arm_module_init, INIT_ARM);
 
 static void arm_task()
 {
+	// configuration des ax12
+	ax12_set_moving_speed(AX12_ARM_1, 0x3ff);
+	ax12_set_moving_speed(AX12_ARM_2, 0x3ff);
+
+	ax12_set_torque_limit(AX12_ARM_1, 0x300);
+	ax12_set_torque_limit(AX12_ARM_2, 0x300);
+
+	ax12_set_torque_enable(AX12_ARM_1, 1);
+	ax12_set_torque_enable(AX12_ARM_2, 1);
+
 	// TODO procedure de mise à zéro
 	arm_z = 0;
 	arm_vz = 0;
@@ -130,7 +140,7 @@ static void arm_cmd_zab(void* arg)
 
 int arm_goto_zab(uint32_t z, int32_t a, int32_t b)
 {
-	log_format(LOG_INFO, "z = %zd a = %zd b = %zd", z, a, b);
+	log_format(LOG_INFO, "z = %d a = %d b = %d", (int)z, (int)a, (int)b);
 
 	ax12_set_goal_position(AX12_ARM_1, a);
 	ax12_set_goal_position(AX12_ARM_2, b);
@@ -150,37 +160,47 @@ int arm_goto_zab(uint32_t z, int32_t a, int32_t b)
 
 static int arm_compute_ab(int32_t x, int32_t y, int32_t* a, int32_t* b)
 {
+	const int32_t sgn = 1; // coude haut ou coude bas
+
 	// verification que l'objectif est dans l'espace de travail
 	int64_t norm2_xy = (int64_t)x * (int64_t)x + (int64_t)y* (int64_t)y;
 	int32_t norm_xy = sqrtf(norm2_xy);
 
-	if( norm_xy > ARM_L1 + ARM_L2 || norm_xy < ARM_L1 - ARM_L2 )
+	// espace accessible, x<0 => dans le robot donc interdit
+	if( x < 0 || norm_xy > ARM_L1 + ARM_L2 || norm_xy < ARM_L1 - ARM_L2 )
 	{
-		log(LOG_ERROR, "espace non accessible");
-		return -1;
+		goto error;
 	}
 
 	int64_t num = norm2_xy - (int64_t)ARM_L1 * (int64_t)ARM_L1 - (int64_t)ARM_L2 * (int64_t)ARM_L2;
-	int32_t cb = (int32_t)(num / ((int64_t)ARM_L1 * (int64_t)ARM_L2)) << 15;
-	*b = fx_acos(cb);
+	int32_t cb = (int32_t)(num / (((int64_t)ARM_L1 * (int64_t)ARM_L2) >> 15));
+	*b = sgn * fx_acos(cb);
 
-	// methode 1
-/*
-	int32_t alpha = fx_atan2(y, x);
-	num = norm2_xy + (int64_t)ARM_L1 * (int64_t)ARM_L1 - (int64_t)ARM_L2 * (int64_t)ARM_L2;
-	den = 2 * ARM_L1 * norm_xy;
-	cb = (int32_t)(num / den);
-	int32_t beta = fx_acos(cb);
+	// prise en compte de la saturation sur b : 150 degrés => 150/360 * 2^26 = 27962026
+	if( abs(*b) > 27962026)
+	{
+		goto error;
+	}
 
-	*a = alpha - beta;
-*/
+	// l1 + l2 * c2
+	int32_t l1_l2c2 = ARM_L1 + (((int64_t)ARM_L2 * (int64_t)cb) >> 16);
+	// l2 * s2
+	int32_t l2s2 = ((int64_t)ARM_L2 * (int64_t)fx_sin(*b)) >> 30;
+	int32_t dx = (((int64_t)x * (int64_t)l1_l2c2) >> 20) + (((int64_t)y * (int64_t)l2s2) >> 20);
+	int32_t dy = (((int64_t)y * (int64_t)l1_l2c2) >> 20) - (((int64_t)x * (int64_t)l2s2) >> 20);
+	*a = fx_atan2(dy, dx);
 
-	// methode 2 (voir si ça marche et si c'est plus rapide)
-	num = (int64_t)(ARM_L1 + (int32_t)(((int64_t)ARM_L2 * (int64_t)cb) >> 16)) * (int64_t)x + (((int64_t)ARM_L2 * fx_sin(*b)) >> 30) * (int64_t)y;
-	int32_t ca = (int32_t)(num / norm2_xy) << 16;
-	*a = fx_acos(ca);
+	// prise en compte de la saturation sur a : 90 degrés => 90/360 * 2^26 = 16777216
+	if( abs(*a) > 16777216)
+	{
+		goto error;
+	}
 
 	return 0;
+
+error:
+	log(LOG_ERROR, "espace non accessible");
+	return -1;
 }
 
 static void arm_cmd_xyz(void* arg)
@@ -192,7 +212,7 @@ static void arm_cmd_xyz(void* arg)
 
 int arm_goto_xyz(int32_t x, int32_t y, uint32_t z)
 {
-	log_format(LOG_INFO, "x = %zd y = %zd z = %zd", x, y, z);
+	log_format(LOG_INFO, "x = %d y = %d z = %d", (int)x, (int)y, (int)z);
 
 	struct fx_vect_pos C_table =
 	{
@@ -213,7 +233,12 @@ int arm_goto_xyz(int32_t x, int32_t y, uint32_t z)
 	int32_t a = 0;
 	int32_t b = 0;
 
-	arm_compute_ab(x, y, &a, &b);
+	int res = arm_compute_ab(C_arm.x, C_arm.y, &a, &b);
+
+	if(res)
+	{
+		return -1;
+	}
 
 	arm_goto_zab(z, a, b);
 
