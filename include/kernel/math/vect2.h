e08b91919ca4a05599320da6c87d54a82088091c
jacen <jacen@atlantronic.fr>
2012-05-15 17:53:30 +0000

    ajout de fonctions de fonctions de calculs sur vecteurs

--- include/kernel/math/vect2.h
+++ include/kernel/math/vect2.h
@@ -14,11 +14,32 @@ struct fx_vect2
 	int32_t y;
 };
 
+//!< @function fx_vect2_sum
+//!< @brief calcule la somme de deux vecteurs u et v
+//!< @param vecteur u
+//!< @param vecteur v
+//!< @return vecteur résultant de u+V
+struct fx_vect2 fx_vect2_sum(const struct fx_vect2* u, const struct fx_vect2* v);
+
+//!< @function fx_vect2_difference
+//!< @brief calcule la différence u-v de deux vecteurs u et v
+//!< @param vecteur u
+//!< @param vecteur v
+//!< @return vecteur résultant de u-v
+struct fx_vect2 fx_vect2_difference(const struct fx_vect2* u, const struct fx_vect2* v);
+
 //!< @function fx_vect2_scalar_product
-////!< @brief calcul le produit scalaire de deux vecteurs u et v
-////!< @param vecteur u
-////!< @param vecteur v
-////!< @return produit scalaire calculé avec les positions en mm
+//!< @brief calcule le produit scalaire de deux vecteurs u et v
+//!< @param vecteur u
+//!< @param vecteur v
+//!< @return produit scalaire calculé avec les positions en mm
 int32_t fx_vect2_scalar_product(const struct fx_vect2* u, const struct fx_vect2* v);
+
+//!< @function fx_vect2_vector_product_z
+//!< @brief calcule la compose sur Oz du produit vectoriel de u et v (appartenant à (Ox,Ox))
+//!< @param vecteur u
+//!< @param vecteur v
+//!< @return composante sur Oz du produit vectoriel calculé avec les positions en mm
+int32_t fx_vect2_vector_product_z(const struct fx_vect2* u, const struct fx_vect2* v);
 
 #endif
--- src/kernel/math/vect2.c
+++ src/kernel/math/vect2.c
@@ -4,6 +4,28 @@
 
 #include "kernel/math/vect2.h"
 
+//!< @function fx_vect2_sum
+//!< @brief calcule la somme de deux vecteurs u et v
+//!< @param vecteur u
+//!< @param vecteur v
+//!< @return vecteur résultant de u+V
+struct fx_vect2 fx_vect2_sum (const struct fx_vect2* u, const struct fx_vect2* v)
+{
+	struct fx_vect2 result = {u->x + v->x, u->y + v->y}; 
+	return result;
+}
+
+//!< @function fx_vect2_difference
+//!< @brief calcule la différence u-v de deux vecteurs u et v
+//!< @param vecteur u
+//!< @param vecteur v
+//!< @return vecteur résultant de u-v
+struct fx_vect2 fx_vect2_difference (const struct fx_vect2* u, const struct fx_vect2* v)
+{
+	struct fx_vect2 result = {u->x - v->x, u->y - v->y}; 
+	return result;
+}
+
 //!< @function fx_vect2_scalar_product
 //!< @brief calcul le produit scalaire de deux vecteurs u et v
 //!< @param vecteur u
@@ -12,5 +34,15 @@
 int32_t fx_vect2_scalar_product(const struct fx_vect2* u, const struct fx_vect2* v)
 {
 	return ((u->x>>16)*(v->x>>16))+((u->y>>16)*(v->y>>16));
+}
+
+//!< @function fx_vect2_vector_product_z
+//!< @brief calcule la compose sur Oz du produit vectoriel de u et v (appartenant à (Ox,Ox))
+//!< @param vecteur u
+//!< @param vecteur v
+//!< @return composante sur Oz du produit vectoriel calculé avec les positions en mm
+int32_t fx_vect2_vector_product_z(const struct fx_vect2* u, const struct fx_vect2* v)
+{
+	return ((u->x>>16)*(v->y>>16))-((u->y>>16)*(v->x>>16));
 }
 
