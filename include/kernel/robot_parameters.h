f711932ffd1c760bd348eefe4cd627c42f67c81f
Trédez Jean-Baptiste <jbtredez@gmail.com>
2012-09-14 19:22:20 +0200

    menage parametres

--- include/kernel/robot_parameters.h
+++ include/kernel/robot_parameters.h
@@ -7,7 +7,6 @@
 
 #include <math.h>
 
-#define PI                              3.141592654f
 #define PI_FX29                           1686629713      //!< pi en 2^-29 rd
 
 #define PARAM_RIGHT_ODO_WHEEL_RADIUS_FX      2588574      //!< rayon de la roue droite en 2^-16 mm (virgule fixe) ( = 39.5 mm)
@@ -20,16 +19,13 @@
 #define PARAM_RIGHT_MOT_WHEEL_WAY                 1
 #define PARAM_LEFT_MOT_WHEEL_WAY                  1
 
-#define PARAM_ENCODERS_RES                     4096
 #define PARAM_ENCODERS_BIT_RES                   12
-
-#define PARAM_MOT_RED                            21
 #define PARAM_ODO_RED                             1
 
 #define PARAM_LEFT_CORNER_X                    190*65536
-#define PARAM_LEFT_CORNER_Y                   -175*65536
+#define PARAM_LEFT_CORNER_Y                    175*65536
 #define PARAM_RIGHT_CORNER_X                   190*65536
-#define PARAM_RIGHT_CORNER_Y                   175*65536
+#define PARAM_RIGHT_CORNER_Y                  -175*65536
 
 #define PARAM_NP_X                            (-63 << 16)
 
@@ -42,20 +38,5 @@
 #define PARAM_BAR_HOKUYO_Y                    -140*65536
 #define PARAM_BAR_HOKUYO_ALPHA                (-1 << 23)
 #define PARAM_BAR_HOKUYO_SENS                          1
-
-// TODO a virer, utiliser les param _FX dans la simulation
-#define PARAM_VOIE_ODO                       301.1526835f
-#define PARAM_VOIE_MOT                       120.0f
-
-#define PARAM_RIGHT_ODO_WHEEL_RADIUS          40.1000061f
-#define PARAM_LEFT_ODO_WHEEL_RADIUS           40.1000061f
-#define PARAM_RIGHT_MOT_WHEEL_RADIUS          50.0f
-#define PARAM_LEFT_MOT_WHEEL_RADIUS           50.0f
-
-// TODO a virer, utiliser les param _FX dans la simulation
-#define PARAM_DIST_ODO_GAIN                (float) (PI / (PARAM_ODO_RED * PARAM_ENCODERS_RES) )
-#define PARAM_ROT_ODO_GAIN                 (float) (2.0f * PI / (PARAM_ENCODERS_RES * PARAM_VOIE_ODO * PARAM_ODO_RED))
-#define PARAM_DIST_MOD_GAIN                (float) (1.0f / (2.0f * PARAM_MOT_RED))
-#define PARAM_ROT_MOD_GAIN                 (float) (1.0f / (PARAM_VOIE_MOT * PARAM_MOT_RED))
 
 #endif
