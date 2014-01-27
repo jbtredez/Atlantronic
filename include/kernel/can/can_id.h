6ed9d9fd6eaa15f625d375af4d859782f77d2c80
Tredez Jean-Baptiste <jbtredez@gmail.com>
2014-01-27 18:22:38 +0100

    can nodeid

--- include/kernel/can/can_id.h
+++ include/kernel/can/can_id.h
@@ -5,11 +5,11 @@
 //! @brief Can - message id
 //! @author Atlantronic
 
-#define CAN_MOTOR_DRIVING1_NODEID      0x20
+#define CAN_MOTOR_DRIVING1_NODEID      0x31
 #define CAN_MOTOR_STEERING1_NODEID     0x21
-#define CAN_MOTOR_DRIVING2_NODEID      0x22
-#define CAN_MOTOR_STEERING2_NODEID     0x23
-#define CAN_MOTOR_DRIVING3_NODEID      0x24
-#define CAN_MOTOR_STEERING3_NODEID     0x25
+#define CAN_MOTOR_DRIVING2_NODEID      0x32
+#define CAN_MOTOR_STEERING2_NODEID     0x22
+#define CAN_MOTOR_DRIVING3_NODEID      0x33
+#define CAN_MOTOR_STEERING3_NODEID     0x23
 
 #endif
