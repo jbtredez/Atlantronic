defc289d305ae38769fff810e2c34ea928f840cf
Tredez Jean-Baptiste <jbtredez@gmail.com>
2014-03-30 11:01:09 +0200

    log gestion puissance et power on par defaut

--- src/kernel/driver/power.c
+++ src/kernel/driver/power.c
@@ -2,7 +2,7 @@
 #include "gpio.h"
 #include "kernel/log.h"
 
-static int power_state = POWER_OFF;
+static int power_state = POWER_ON;
 
 void power_set(int powerEventMask)
 {
@@ -12,12 +12,18 @@ void power_set(int powerEventMask)
 	{
 		gpio_power_off();
 	}
-	if(old_state !=  power_state)
+
+	int diff = power_state ^ old_state;
+	if( diff & POWER_OFF_UNDERVOLTAGE )
 	{
-		if( power_state & POWER_OFF_UNDERVOLTAGE )
-		{
-			log(LOG_INFO, "undervoltage");
-		}
+		log(LOG_ERROR, "power off - undervoltage");
+	}
+	if( diff & POWER_OFF_END_MATCH )
+	{
+		log(LOG_INFO, "power off - end match");
+	}
+	if( diff & POWER_OFF )
+	{
 		log(LOG_INFO, "power off");
 	}
 }
@@ -26,12 +32,27 @@ void power_clear(int powerEventMask)
 {
 	int old_state = power_state;
 	power_state &= ~powerEventMask;
+
+	int diff = powerEventMask & old_state;
+	if( diff & POWER_OFF_UNDERVOLTAGE )
+	{
+		log(LOG_INFO, "power clear - undervoltage");
+	}
+	if( diff & POWER_OFF_END_MATCH )
+	{
+		log(LOG_INFO, "power clear - end match");
+	}
+	if( diff & POWER_OFF )
+	{
+		log(LOG_INFO, "power clear - off");
+	}
+
 	if( ! power_state )
 	{
 		gpio_power_on();
-	}
-	if(old_state !=  power_state)
-	{
-		log(LOG_INFO, "power on");
+		if(old_state != power_state)
+		{
+			log(LOG_INFO, "power on");
+		}
 	}
 }
