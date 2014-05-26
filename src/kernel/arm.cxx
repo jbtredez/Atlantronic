aa8cb69bbcacfa5c4d4aefc814bb3f946c02cdbc
Tredez Jean-Baptiste <jbtredez@gmail.com>
2014-05-26 10:41:54 +0200

    arm - parametrage

--- src/kernel/arm.cxx
+++ src/kernel/arm.cxx
@@ -25,16 +25,23 @@ static struct arm_cmd arm_pos_cmd; //!< position commandee du bras
 static struct arm_cmd arm_pos_mes;
 static xSemaphoreHandle arm_mutex;
 static int arm_cmd_type;
+static bool arm_target_reached = false;
+
 static void arm_disabled_run();
 static unsigned int arm_disabled_transition(unsigned int currentState);
 
-static void arm_homing_run();
+static void arm_homing_entry();
+static void arm_homing0_run();
+static void arm_homing1_run();
+static void arm_homing2_run();
 static unsigned int arm_homing_transition(unsigned int currentState);
 
 static MatrixHomogeneous arm_transform;
 static StateMachineState arm_states[ARM_STATE_MAX] = {
 		{ "ARM_STATE_DISABLED", &nop_function, &arm_disabled_run, &arm_disabled_transition},
-		{ "ARM_STATE_HOMING", &nop_function, &arm_homing_run, &arm_homing_transition},
+		{ "ARM_STATE_HOMING_0", &arm_homing_entry, &arm_homing0_run, &arm_homing_transition},
+		{ "ARM_STATE_HOMING_1", &arm_homing_entry, &arm_homing1_run, &arm_homing_transition},
+		{ "ARM_STATE_HOMING_2", &arm_homing_entry, &arm_homing2_run, &arm_homing_transition},
 };
 static StateMachine arm_stateMachine(arm_states, ARM_STATE_MAX);
 
@@ -59,12 +66,21 @@ static int arm_module_init()
 		return ERR_INIT_ARM;
 	}
 
-	// TODO
 	ax12.set_goal_limit(AX12_ARM_SHOULDER, -M_PI_2, M_PI_2);
 	ax12.set_goal_limit(AX12_ARM_SHOULDER_ELBOW, -M_PI_2, M_PI_2);
 	ax12.set_goal_limit(AX12_ARM_WRIST_ELBOW, -M_PI_2, M_PI_2);
 	ax12.set_goal_limit(AX12_ARM_WRIST, -M_PI_2, M_PI_2);
-	rx24.set_goal_limit(RX24_ARM_SLIDER, -M_PI_2, M_PI_2);
+	rx24.set_goal_limit(RX24_ARM_SLIDER, -0.28, 0.98);
+
+	ax12.set_torque_limit(AX12_ARM_SHOULDER, 1);
+	ax12.set_torque_limit(AX12_ARM_SHOULDER_ELBOW, 0.5);
+	ax12.set_torque_limit(AX12_ARM_WRIST_ELBOW, 0.5);
+	ax12.set_torque_limit(AX12_ARM_WRIST, 0.5);
+	rx24.set_torque_limit(RX24_ARM_SLIDER, 0.8);
+
+	ax12.set_target_reached_threshold(AX12_ARM_SHOULDER, 0.05);
+	ax12.set_target_reached_threshold(AX12_ARM_WRIST_ELBOW, 0.035);
+	rx24.set_target_reached_threshold(RX24_ARM_SLIDER, 0.05);
 
 	usb_add_cmd(USB_CMD_ARM, &arm_cmd);
 
@@ -76,17 +92,6 @@ module_init(arm_module_init, INIT_ARM);
 static void arm_task(void* arg)
 {
 	(void) arg;
-/*
-	// configuration des ax12
-	ax12_set_moving_speed(AX12_ARM_1, 0x3ff);
-	ax12_set_moving_speed(AX12_ARM_2, 0x3ff);
-
-	ax12_set_torque_limit(AX12_ARM_1, 0x300);
-	ax12_set_torque_limit(AX12_ARM_2, 0x300);
-
-	ax12_set_torque_enable(AX12_ARM_1, 1);
-	ax12_set_torque_enable(AX12_ARM_2, 1);
-*/
 
 	while(1)
 	{
@@ -142,6 +147,17 @@ static void arm_update_position()
 		arm_transform.translate(ARM_DIST_WRIST_ELBOW_TO_WRIST, 0, 0);
 		arm_transform.rotateZ( arm_pos_mes.val[ARM_AXIS_WRIST] );
 		arm_transform.translate(ARM_DIST_WRIST_TO_SUCKER, 0, 0);
+
+		arm_target_reached = ax12.isFlagActive(AX12_ARM_SHOULDER, DYNAMIXEL_FLAG_TARGET_REACHED);
+		arm_target_reached &= ax12.isFlagActive(AX12_ARM_SHOULDER_ELBOW, DYNAMIXEL_FLAG_TARGET_REACHED);
+		arm_target_reached &= ax12.isFlagActive(AX12_ARM_WRIST_ELBOW, DYNAMIXEL_FLAG_TARGET_REACHED);
+		arm_target_reached &= ax12.isFlagActive(AX12_ARM_WRIST, DYNAMIXEL_FLAG_TARGET_REACHED);
+		// TODO bug ?
+		//arm_target_reached &= rx24.isFlagActive(RX24_ARM_SLIDER, DYNAMIXEL_FLAG_TARGET_REACHED);
+	}
+	else
+	{
+		arm_target_reached = false;
 	}
 }
 /*
@@ -179,32 +195,72 @@ static unsigned int arm_disabled_transition(unsigned int currentState)
 {
 	if( ! power_get() && arm_cmd_type == ARM_CMD_HOMING )
 	{
-		return ARM_STATE_HOMING;
+		return ARM_STATE_HOMING_0;
 	}
 
 	return currentState;
 }
 
 //---------------------- Etat ARM_STATE_HOMING --------------------------------
-static void arm_homing_run()
+static void arm_homing_entry()
 {
-	// TODO mesurer la position homing + voir si on bouge les servos 1 par 1
+	arm_target_reached = false;
+}
+
+static void arm_homing0_run()
+{
 	arm_pos_cmd.val[ARM_AXIS_SHOULDER] = 0;
 	arm_pos_cmd.val[ARM_AXIS_SHOULDER_ELBOW] = 0;
 	arm_pos_cmd.val[ARM_AXIS_WRIST_ELBOW] = 0;
 	arm_pos_cmd.val[ARM_AXIS_WRIST] = 0;
+	arm_pos_cmd.val[ARM_AXIS_SLIDER] = arm_pos_mes.val[ARM_AXIS_SLIDER];
 
 	arm_update();
 }
 
-static unsigned int arm_homing_transition(unsigned int currentState)
+static void arm_homing1_run()
+{
+	arm_pos_cmd.val[ARM_AXIS_SHOULDER] = 0;
+	arm_pos_cmd.val[ARM_AXIS_SHOULDER_ELBOW] = 0;
+	arm_pos_cmd.val[ARM_AXIS_WRIST_ELBOW] = 0;
+	arm_pos_cmd.val[ARM_AXIS_WRIST] = 0;
+	arm_pos_cmd.val[ARM_AXIS_SLIDER] = arm_pos_mes.val[ARM_AXIS_SLIDER]; // TODO val
+
+	arm_update();
+}
+
+static void arm_homing2_run()
+{
+	arm_pos_cmd.val[ARM_AXIS_SHOULDER] = M_PI/2;
+	arm_pos_cmd.val[ARM_AXIS_SHOULDER_ELBOW] = 0;
+	arm_pos_cmd.val[ARM_AXIS_WRIST_ELBOW] = 0;
+	arm_pos_cmd.val[ARM_AXIS_WRIST] = 0;
+	arm_pos_cmd.val[ARM_AXIS_SLIDER] = arm_pos_mes.val[ARM_AXIS_SLIDER]; // TODO val
+
+	arm_update();
+}
+
+static unsigned int arm_homing_transition(unsigned int state)
 {
 	if( power_get() || arm_cmd_type == ARM_CMD_DISABLE )
 	{
 		return ARM_STATE_DISABLED;
 	}
 
-	return currentState;
+	if( state == ARM_STATE_HOMING_0 && arm_target_reached )
+	{
+		state = ARM_STATE_HOMING_1;
+	}
+	else if( state == ARM_STATE_HOMING_1 && arm_target_reached )
+	{
+		state = ARM_STATE_HOMING_2;
+	}
+	else if( state == ARM_STATE_HOMING_1 && arm_target_reached )
+	{
+		//state = ARM_STATE_ENABLE; // TODO
+	}
+
+	return state;
 }
 
 //---------------------- Fin StateMachine -------------------------------------
