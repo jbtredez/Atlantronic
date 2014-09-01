e6836b6fd348979348d79371a340a336e36154fd
Tredez Jean-Baptiste <jbtredez@gmail.com>
2014-09-01 22:06:05 +0200

    refactoring code io

--- src/discovery/bsp.c
+++ src/discovery/bsp.c
@@ -19,47 +19,47 @@ static xQueueHandle gpio_queue_go;
 static void gpio_cmd_go(void* arg);
 static void gpio_cmd_color(void* arg);
 
+typedef struct
+{
+	GPIO_TypeDef* gpio;
+	uint32_t pin;
+	enum gpio_pupd pupd;
+}BspIo;
+
+
+// io sur les connecteurs io generiques
+static const BspIo bsp_io[] =
+{
+	{GPIOD, 11, GPIO_PUPD_UP}, // PD11 : IN_1
+	{GPIOB, 13, GPIO_PUPD_UP}, // PB13 : IN_2
+	{GPIOB, 12, GPIO_PUPD_UP}, // PB12 : IN_3
+	{GPIOD, 10, GPIO_PUPD_UP}, // PD10 : IN_4
+	{GPIOD,  7, GPIO_PUPD_UP}, // PD7  : IN_5
+	{GPIOB, 11, GPIO_PUPD_UP}, // PB11 : IN_6
+	{GPIOC, 11, GPIO_PUPD_UP}, // PC11 : IN_7
+	{GPIOD,  6, GPIO_PUPD_UP}, // PD6  : IN_8
+	{GPIOC,  9, GPIO_PUPD_UP}, // PC9  : IN_9
+	{GPIOC,  8, GPIO_PUPD_UP}, // PC8  : IN_10
+	{GPIOB, 14, GPIO_PUPD_UP}, // PB14 : IN_11
+	{GPIOB, 15, GPIO_PUPD_UP}, // PB15 : IN_12
+	{GPIOE,  6, GPIO_PUPD_UP}, // PE6  : IN_13
+	{GPIOE,  5, GPIO_PUPD_UP}, // PE5  : IN_14
+	{GPIOB,  7, GPIO_PUPD_DOWN}, // bouton USR2
+	{GPIOC, 14, GPIO_PUPD_DOWN}, // bouton USR1
+	{GPIOD,  3, GPIO_PUPD_DOWN}, // bouton go
+};
+
 static int gpio_module_init(void)
 {
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOEEN;
 
 	// "io entrees"
-	// boutons USR1 et USR2 carte led sur PC14 et PB7
-	// bouton go sur PD3
-	gpio_pin_init(GPIOB, 7, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_DOWN); // bouton USR2
-	gpio_pin_init(GPIOC, 14, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_DOWN); // bouton USR1
-	gpio_pin_init(GPIOD, 3, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_DOWN); // bouton go
-
-	// io sur les 5 connecteurs io generiques
-	// pull up pour les omron
-	// PD11 : IN_1
-	// PB13 : IN_2
-	// PB12 : IN_3
-	// PD10 : IN_4
-	// PD7  : IN_5
-	// PB11 : IN_6
-	// PC11 : IN_7
-	// PD6  : IN_8
-	// PC9  : IN_9
-	// PC8  : IN_10
-	// PB14 : IN_11
-	// PB15 : IN_12
-	// PE6  : IN_13
-	// PE5  : IN_14
-	gpio_pin_init(GPIOD, 11, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP); // IN_1
-	gpio_pin_init(GPIOB, 13, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP); // IN_2
-	gpio_pin_init(GPIOB, 12, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP); // IN_3
-	gpio_pin_init(GPIOD, 10, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP); // IN_4
-	gpio_pin_init(GPIOD, 7, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP);  // IN_5
-	gpio_pin_init(GPIOB, 11, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP); // IN_6
-	gpio_pin_init(GPIOC, 11, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP); // IN_7
-	gpio_pin_init(GPIOD, 6, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP);  // IN_8
-	gpio_pin_init(GPIOC, 9, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP);  // IN_9
-	gpio_pin_init(GPIOC, 8, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP);  // IN_10
-	gpio_pin_init(GPIOB, 14, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP);  // IN_11
-	gpio_pin_init(GPIOB, 15, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP);  // IN_12
-	gpio_pin_init(GPIOE, 6, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP);  // IN_13
-	gpio_pin_init(GPIOE, 5, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, GPIO_PUPD_UP);  // IN_14
+	unsigned int i;
+	for(i = 0; i < sizeof(bsp_io)/sizeof(bsp_io[0]); i++)
+	{
+		// pull up pour les omron
+		gpio_pin_init(bsp_io[i].gpio, bsp_io[i].pin, GPIO_MODE_IN, GPIO_SPEED_50MHz, GPIO_OTYPE_PP, bsp_io[i].pupd);
+	}
 
 	color = COLOR_UNKNOWN;
 	gpio_go = 0;
@@ -95,24 +95,13 @@ uint32_t gpio_get_state()
 {
 	uint32_t res = 0;
 
-	res |= gpio_get_pin(GPIOD, 11);      // IN_1
-	res |= gpio_get_pin(GPIOB, 13) << 1; // IN_2
-	res |= gpio_get_pin(GPIOB, 12) << 2; // IN_3
-	res |= gpio_get_pin(GPIOD, 10) << 3; // IN_4
-	res |= gpio_get_pin(GPIOD, 7) << 4;  // IN_5
-	res |= gpio_get_pin(GPIOB, 11) << 5; // IN_6
-	res |= gpio_get_pin(GPIOC, 11) << 6; // IN_7
-	res |= gpio_get_pin(GPIOD, 6) << 7;  // IN_8
-	res |= gpio_get_pin(GPIOC, 9) << 8;  // IN_9
-	res |= gpio_get_pin(GPIOC, 8) << 9;  // IN_10
-	res |= gpio_get_pin(GPIOB, 14) << 10;  // IN_11
-	res |= gpio_get_pin(GPIOB, 15) << 11;  // IN_12
-	res |= gpio_get_pin(GPIOE, 6) << 12;  // IN_13
-	res |= gpio_get_pin(GPIOE, 5) << 13;  // IN_14
-	res |= gpio_get_pin(GPIOC, 14) << 14;  // IN_BTN1
-	res |= gpio_get_pin(GPIOB, 7) << 15;  // IN_BTN2
-	res |= gpio_get_pin(GPIOD, 3) << 16; // INGO
-	res |= (gpio_go?1:0) << 17; // GO
+	unsigned int i = 0;
+	for(i = 0; i < sizeof(bsp_io)/sizeof(bsp_io[0]); i++)
+	{
+		res |= gpio_get_pin(bsp_io[i].gpio, bsp_io[i].pin) << i;
+	}
+
+	res |= (gpio_go?1:0) << i; // GO
 
 	return res;
 }
