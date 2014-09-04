515b290a44dda5cee9a945cb6f59c12b29bd266d
Tredez Jean-Baptiste <jbtredez@gmail.com>
2014-09-04 07:37:50 +0200

    led - rangement define

--- src/discovery/bsp.h
+++ src/discovery/bsp.h
@@ -12,14 +12,6 @@
 extern "C" {
 #endif
 
-#define LED_CPU_RED      0x00004000
-#define LED_CPU_BLUE     0x00008000
-#define LED_EXT_BLUE     0x80000000
-#define LED_EXT_GREEN    0x20000000
-#define LED_EXT_ORANGE1  0x00100000
-#define LED_EXT_ORANGE2  0x00040000
-#define LED_EXT_RED      0x01000000
-
 #define COLOR_UNKNOWN     0
 #define COLOR_RED         1
 #define COLOR_YELLOW      2
--- src/kernel/led.h
+++ src/kernel/led.h
@@ -1,6 +1,14 @@
 #ifndef LED_H
 #define LED_H
 
+#define LED_CPU_RED      0x00004000
+#define LED_CPU_BLUE     0x00008000
+#define LED_EXT_BLUE     0x80000000
+#define LED_EXT_GREEN    0x20000000
+#define LED_EXT_ORANGE1  0x00100000
+#define LED_EXT_ORANGE2  0x00040000
+#define LED_EXT_RED      0x01000000
+
 void setLed(uint32_t mask);
 
 #endif
