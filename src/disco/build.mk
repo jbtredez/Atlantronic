10d4326f18e5cf0a74b625888029805bec9ca683
Tredez Jean-Baptiste <jbtredez@gmail.com>
2014-09-17 22:51:02 +0200

    build - rangement + qemu

--- src/disco/build.mk
+++ src/disco/build.mk
@@ -9,6 +9,7 @@ core_os += kernel/list.o
 core_os += kernel/heap_1.o
 core_os += kernel/fault.o
 core_os += kernel/driver/gpio.o
+core_os += kernel/log.o
 
 core_usb += kernel/driver/usb/usb.o
 core_usb += kernel/driver/usb/usb_descriptor.o
@@ -20,22 +21,28 @@ core_usb += kernel/driver/usb/stm32f4xx/usbd_ctlreq.o
 core_usb += kernel/driver/usb/stm32f4xx/usbd_ioreq.o
 core_usb += kernel/driver/usb/stm32f4xx/usbd_conf.o
 
+core_cpu_board += kernel/driver/sdram.o
+core_cpu_board += kernel/driver/spi.o
+core_cpu_board += kernel/driver/gyro/l3gd20.o
+core_cpu_board += kernel/driver/lcd/lcd.o
+core_cpu_board += kernel/driver/i2c.o
+core_cpu_board += kernel/driver/exti.o
+core_cpu_board += kernel/driver/ts/stmpe811.o
+
 obj-disco-core_os += ${core_os}
 bin-disco += core_os
 
+obj-disco-core_cpu_board += ${core_os}
+obj-disco-core_cpu_board += ${core_usb}
+obj-disco-core_cpu_board += ${core_cpu_board}
+bin-disco += core_cpu_board
+
 obj-disco-core += ${core_os}
 obj-disco-core += ${core_usb}
-obj-disco-core += kernel/log.o
-obj-disco-core += kernel/driver/sdram.o
+obj-disco-core += ${core_cpu_board}
 obj-disco-core += kernel/driver/pwm.o
-obj-disco-core += kernel/driver/spi.o
-obj-disco-core += kernel/driver/gyro/l3gd20.o
-obj-disco-core += kernel/driver/lcd/lcd.o
 obj-disco-core += kernel/driver/encoder.o
-obj-disco-core += kernel/driver/i2c.o
-obj-disco-core += kernel/driver/ts/stmpe811.o
 obj-disco-core += kernel/driver/power.o
-obj-disco-core += kernel/driver/exti.o
 obj-disco-core += kernel/driver/adc.o
 obj-disco-core += kernel/driver/io.o
 obj-disco-core += kernel/match.o
