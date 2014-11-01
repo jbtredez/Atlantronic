5b9dd540322e29045e59cdbe2827fb904a6de409
Tredez Jean-Baptiste <jbtredez@gmail.com>
2014-11-01 14:14:10 +0100

    ajout elements

--- src/linux/tools/table3d.cxx
+++ src/linux/tools/table3d.cxx
@@ -6,6 +6,33 @@ bool Table3d::init()
 	res &= dispenser.init("media/distributeur.obj");
 	res &= clapYellow.init("media/clap_jaune.obj");
 	res &= clapGreen.init("media/clap_vert.obj");
+	res &= feet.init("media/pied.obj");
+	res &= glass.init("media/verre.obj");
+
+	feetPosition[0] = aiVector3D(-1410, -850, 0);
+	feetPosition[1] = aiVector3D(-1410, -750, 0);
+	feetPosition[2] = aiVector3D(-1410,  800, 0);
+	feetPosition[3] = aiVector3D(- 650,  800, 0);
+	feetPosition[4] = aiVector3D(- 650,  900, 0);
+	feetPosition[5] = aiVector3D(- 630, -355, 0);
+	feetPosition[6] = aiVector3D(- 400, -750, 0);
+	feetPosition[7] = aiVector3D(- 200, -400, 0);
+
+	feetPosition[8]  = aiVector3D(1410, -850, 0);
+	feetPosition[9]  = aiVector3D(1410, -750, 0);
+	feetPosition[10] = aiVector3D(1410,  800, 0);
+	feetPosition[11] = aiVector3D( 650,  800, 0);
+	feetPosition[12] = aiVector3D( 650,  900, 0);
+	feetPosition[13] = aiVector3D( 630, -355, 0);
+	feetPosition[14] = aiVector3D( 400, -750, 0);
+	feetPosition[15] = aiVector3D( 200, -400, 0);
+
+	glassPosition[0] = aiVector3D(-1250, -750, 0);
+	glassPosition[1] = aiVector3D(- 590,  170, 0);
+	glassPosition[2] = aiVector3D(    0, -650, 0);
+	glassPosition[3] = aiVector3D(  590,  170, 0);
+	glassPosition[4] = aiVector3D( 1250, -750, 0);
+
 	return res;
 }
 
@@ -28,6 +55,9 @@ void Table3d::draw()
 	drawClap(  500, false);
 	drawClap(  800, true);
 	drawClap( 1100, false);
+
+	drawFeets();
+	drawGlass();
 }
 
 void Table3d::drawDispenser(float x)
@@ -63,4 +93,26 @@ void Table3d::drawClap(float x, bool yellow)
 	}
 	clap->draw();
 	glPopMatrix();
+}
+
+void Table3d::drawFeets()
+{
+	for(unsigned int i = 0; i < sizeof(feetPosition) / sizeof(feetPosition[0]); i++)
+	{
+		glPushMatrix();
+		glTranslatef( -feet.sceneCenter.x + feetPosition[i].x, -feet.sceneCenter.y + feetPosition[i].y, -feet.sceneMin.z + feetPosition[i].z );
+		feet.draw();
+		glPopMatrix();
+	}
+}
+
+void Table3d::drawGlass()
+{
+	for(unsigned int i = 0; i < sizeof(glassPosition) / sizeof(glassPosition[0]); i++)
+	{
+		glPushMatrix();
+		glTranslatef( -glass.sceneCenter.x + glassPosition[i].x, -glass.sceneCenter.y + glassPosition[i].y, -glass.sceneMin.z + glassPosition[i].z );
+		glass.draw();
+		glPopMatrix();
+	}
 }
--- src/linux/tools/table3d.h
+++ src/linux/tools/table3d.h
@@ -12,11 +12,17 @@ class Table3d
 	protected:
 		void drawDispenser(float x);
 		void drawClap(float x, bool yellow);
+		void drawFeets();
+		void drawGlass();
 
 		Object3d table;
 		Object3d dispenser;
 		Object3d clapYellow;
 		Object3d clapGreen;
+		Object3d feet;
+		Object3d glass;
+		aiVector3D feetPosition[16];
+		aiVector3D glassPosition[5];
 };
 
 #endif
