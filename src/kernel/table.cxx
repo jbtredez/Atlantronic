5e713b6f8195f01aff93064efee7c277eed31d09
Tredez Jean-Baptiste <jbtredez@gmail.com>
2015-03-27 19:40:27 +0100

    ajout estrade dans la carte du robot

--- src/kernel/table.cxx
+++ src/kernel/table.cxx
@@ -8,6 +8,11 @@ const struct Vect2 table_contour[] =
 	Vect2( -1500, -1000), Vect2( -1500, 1000 ), Vect2( 1500, 1000 ), Vect2( 1500, -1000 ), Vect2( -1500, -1000 )
 };
 
+const struct Vect2 table_estrade[] =
+{
+	Vect2( -300, -1000), Vect2( -300, -900 ), Vect2( 300, -900 ), Vect2( 300, -1000 )
+};
+
 const struct Vect2 table_bordure_marches[] =
 {
 	Vect2(-533, 1000), Vect2(-533, 410), Vect2(-511, 410), Vect2(-511, 978), Vect2(-11, 978), Vect2(-11, 410), Vect2(11, 410), Vect2(11, 978), Vect2(511, 978), Vect2(511, 410), Vect2(533, 410), Vect2(533, 1000)
@@ -36,6 +41,7 @@ const struct Vect2 table_depart_jaune[] =
 const struct polyline table_obj[TABLE_OBJ_SIZE] =
 {
 	{ (Vect2*) table_contour, ARRAY_SIZE(table_contour) },
+	{ (Vect2*) table_estrade, ARRAY_SIZE(table_estrade) },
 	{ (Vect2*) table_bordure_marches, ARRAY_SIZE(table_bordure_marches) },
 	{ (Vect2*) table_marches_jaunes, ARRAY_SIZE(table_marches_jaunes) },
 	{ (Vect2*) table_marches_vertes, ARRAY_SIZE(table_marches_vertes) },
