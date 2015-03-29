b6ebde785d45325ef53d74fa66fd037faccee097
Tredez Jean-Baptiste <jbtredez@gmail.com>
2015-03-29 16:20:09 +0200

    compilation fpu hw ou soft pour debug

--- src/kernel/asm/isr_stm32f4xx.c
+++ src/kernel/asm/isr_stm32f4xx.c
@@ -313,9 +313,11 @@ void isr_context_switch( void )
 		"	ldr	r3, pxCurrentTCBConst			\n" /* Get the location of the current TCB. */
 		"	ldr	r2, [r3]						\n"
 		"										\n"
+#if defined (__VFP_FP__) && !defined(__SOFTFP__)
 		"	tst r14, #0x10						\n" /* Is the task using the FPU context?  If so, push high vfp registers. */
 		"	it eq								\n"
 		"	vstmdbeq r0!, {s16-s31}				\n"
+#endif
 		"										\n"
 		"	stmdb r0!, {r4-r11, r14}			\n" /* Save the core registers. */
 		"										\n"
@@ -334,9 +336,11 @@ void isr_context_switch( void )
 		"										\n"
 		"	ldmia r0!, {r4-r11, r14}			\n" /* Pop the core registers. */
 		"										\n"
+#if defined (__VFP_FP__) && !defined(__SOFTFP__)
 		"	tst r14, #0x10						\n" /* Is the task using the FPU context?  If so, pop the high vfp registers too. */
 		"	it eq								\n"
 		"	vldmiaeq r0!, {s16-s31}				\n"
+#endif
 		"										\n"
 		"	msr psp, r0							\n"
 		"	bx r14								\n"
