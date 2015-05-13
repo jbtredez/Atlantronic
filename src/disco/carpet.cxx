f9262baa47a7fff34a72ad86a93e729944a9dd54
Tredez Jean-Baptiste <jbtredez@gmail.com>
2015-05-13 10:34:17 +0200

    tapis sans les H (a verifier si ok avec tapis PS)

--- src/disco/carpet.cxx
+++ src/disco/carpet.cxx
@@ -31,7 +31,7 @@ void carpet_set_pos(enum carpet_type right, enum carpet_type left)
 			ax12.set_goal_position(AX12_RIGHT_CARPET, 0);
 			break;
 		case CARPET_DOWN:
-			ax12.set_goal_position(AX12_RIGHT_CARPET, -1.4);
+			ax12.set_goal_position(AX12_RIGHT_CARPET, -1);
 			break;
 		case CARPET_NO_MOVE:
 		default:
@@ -44,7 +44,7 @@ void carpet_set_pos(enum carpet_type right, enum carpet_type left)
 			ax12.set_goal_position(AX12_LEFT_CARPET, 0);
 			break;
 		case CARPET_DOWN:
-			ax12.set_goal_position(AX12_LEFT_CARPET, 1.4);
+			ax12.set_goal_position(AX12_LEFT_CARPET, 1);
 			break;
 		case CARPET_NO_MOVE:
 		default:
