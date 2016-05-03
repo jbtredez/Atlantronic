aad4012a04ac19f8f81b8795e06d8b3647a064cd
Julien Viaud <julien.viaudfr@gmail.com>
2016-05-03 11:36:52 +0200

    -Mise à jour des Params d'asservissement

--- src/disco/gate/gate.h
+++ src/disco/gate/gate.h
@@ -26,21 +26,21 @@
 #define GATE_YKD									0
 #define GATE_YMAX									1 // TODO voir saturation + regler
 
-#define GATE_THETAKP								1
+#define GATE_THETAKP								0
 #define GATE_THETAKI								0
 #define GATE_THETAKD								0
 #define GATE_THETAMAX								1 // TODO voir saturation
 
 // Parametres des roues
 #define GATE_VOIE_MOT                            144.0f
-#define GATE_VOIE_ODO                             92.0f
-#define GATE_DRIVING1_WHEEL_RADIUS                90.0f
+#define GATE_VOIE_ODO                             92.36f
+#define GATE_DRIVING1_WHEEL_RADIUS                90.1f
 #define GATE_DRIVING2_WHEEL_RADIUS                90.7f
 #define GATE_MOTOR_DRIVING1_RED             -(78/10.0f)  //!< reduction moteur 1
 #define GATE_MOTOR_DRIVING2_RED              (78/10.0f)  //!< reduction moteur 2
 #define GATE_MOTOR_RPM_TO_VOLT               (3/163.5f)
-#define GATE_ODO1_WHEEL_RADIUS                    38.7f
-#define GATE_ODO2_WHEEL_RADIUS                    38.7f
+#define GATE_ODO1_WHEEL_RADIUS                    38.89f
+#define GATE_ODO2_WHEEL_RADIUS                    38.89f
 #define GATE_ODO1_WAY                                 1
 #define GATE_ODO2_WAY                                -1
 #define GATE_ODO_ENCODER_RESOLUTION                4096
