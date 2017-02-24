45b343426f9ddf6a2883672b37ad5877e31e6f6d
Tredez Jean-Baptiste <jbtredez@gmail.com>
2017-02-24 19:28:41 +0100

    code de test esc sur bouton couleur

--- src/kernel/driver/esc.cxx
+++ src/kernel/driver/esc.cxx
@@ -36,18 +36,27 @@ int esc_module_init()
 
 module_init(esc_module_init, INIT_GYRO);
 
+static Systime esc_t1;
+static Systime esc_t2;
+static Systime esc_dt1;
+static Systime esc_dt2;
+static Systime esc_dt;
+
 extern "C"
 {
 void isr_tim7(void)
 {
-	// generation d'un signal PPM : periode de 20ms
-	// haut pendant 1ms, bas 19ms => 0%
-	// haut pendant 2ms, bas 18ms => 100%
+	// generation d'un signal PPM
 	if( TIM7->SR | TIM_SR_UIF )
  	{
+		esc_val = gpio_get(GPIO_1);
 		if( esc_val < 0)
 		{
 			esc_val = 0;
+		}
+		if( esc_val > 1 )
+		{
+			esc_val = 1;
 		}
 		// TODO limitation a 25% pour le moment
 		if( esc_val > 0.25 )
@@ -55,18 +64,25 @@ void isr_tim7(void)
 			esc_val = 0.25;
 		}
 
-		if( esc_remain_count == ESP_PERIOD )
+		if( ! esc_remain_count )
 		{
 			int val = 700 + 1000 * esc_val; // entre 0.7 et 1.7 ms
 			TIM7->ARR = val;
-			esc_remain_count -= val;
+			esc_remain_count = ESP_PERIOD - val;
 			gpio_set(GPIO_11);
+			Systime t = systick_get_time_from_isr();
+			esc_dt1 = t - esc_t1;
+			esc_t1 = t;
 		}
 		else
 		{
 			TIM7->ARR = esc_remain_count;
-			esc_remain_count = ESP_PERIOD;
+			esc_remain_count = 0;
 			gpio_reset(GPIO_11);
+			Systime t = systick_get_time_from_isr();
+			esc_dt2 = t - esc_t2;
+			esc_t2 = t;
+			esc_dt = esc_t2 - esc_t1;
 		}
 		TIM7->SR &= ~TIM_SR_UIF;
 	}
