38980744d184b55e06e22cae5a0d090b3c664a98
Tredez Jean-Baptiste <jbtredez@gmail.com>
2017-05-14 20:16:34 +0200

    Star : suppression moteur pap

--- src/disco/star/star.mk
+++ src/disco/star/star.mk
@@ -3,7 +3,6 @@ star_robot += disco/star/power.o
 star_robot += disco/star/servos.o
 star_robot += kernel/CanMipNode.o
 star_robot += kernel/CanMipMotor.o
-star_robot += kernel/driver/StepperDriver.o
 star_robot += disco/star/recalage.o
 star_robot += kernel/driver/esc.o
 
