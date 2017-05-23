586410a9f5dc1dc5cf1130dba1cff9dc45e8df24
Tredez Jean-Baptiste <jbtredez@gmail.com>
2017-05-23 10:25:37 +0200

    turbine sur gate

--- src/kernel/driver/esc.h
+++ src/kernel/driver/esc.h
@@ -6,11 +6,10 @@
 //! @author Atlantronic
 
 #include <stdint.h>
-static void cmd_esc(void* arg, void* data);
 
 class Esc
 {
 	public:
-		void SetVal(float cmd);
+		void setVal(float cmd);
 };
 #endif
